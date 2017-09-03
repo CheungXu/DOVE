c2a0ff4b08722d0b67f99b23115fdd0593459576
CheungXu <cheungxu_0218@foxmail.com>
2017-09-03 18:04:33 +0800

    ZX-2017.09.03
    1.experiment.py面向对象化
    2.dove.py中suitor的search轮数从固定进行50轮停止更新至状态无改变时停止。

---
+++ experiment.py
@@ -0,0 +1,76 @@
+import dove
+import copy
+
+class experiment(object):
+    def __init__(self,person_num = 20):
+        self.person_num = person_num
+        self.save_path = '.\\data\\test.txt'
+        self.save = open(self.save_path,'w')
+        random = dove.list_randomer(0,self.person_num)
+        self.mans_list = []
+        self.womans_list = []
+        for i in range(self.person_num):
+            self.mans_list.append(copy.copy(random.create_list()))
+            self.womans_list.append(copy.copy(random.create_list()))
+
+    def end(self):
+        self.person_num  = 0
+        self.save_path = ''
+        self.save.close()
+        self.mans_list = []
+        self.womans_list = []
+        
+    def write_init_condition(self):
+        self.save.write('MAN_LIST\n')
+        for i in range(len(self.mans_list)):
+            for j in range(len(self.mans_list[i])):
+                self.save.write(str(self.mans_list[i][j]))
+                self.save.write(' ')
+            self.save.write('\n')
+        self.save.write('\nWOMAN_LIST\n')
+        for i in range(len(self.womans_list)):
+            for j in range(len(self.womans_list[i])):
+                self.save.write(str(self.womans_list[i][j]))
+                self.save.write(' ')
+            self.save.write('\n')
+
+    def __unidirectional_match__(self,suitor,receiver):
+        suitor_list = copy.deepcopy(suitor)
+        receiver_list = copy.deepcopy(receiver)
+        suitor_result = dove.create_Suitors(suitor_list)
+        receiver_result = dove.create_Receivers(receiver_list)
+        experiment = dove.experiment(suitor_result,receiver_result)
+        experiment.start()
+        return suitor_result,receiver_result,experiment
+
+    def unidirectional_match_both_sides(self):
+        suitor_man,receiver_woman,exp1 = self.__unidirectional_match__(self.mans_list,self.womans_list)
+        suitor_woman,receiver_man,exp2 = self.__unidirectional_match__(self.womans_list,self.mans_list)
+
+        self.save.write('\nEXP ONE \n')
+        self.save.write('SUI_MAN\n')
+        exp1.save_suitors(self.save)
+        self.save.write('REC_WOMAN\n')
+        exp1.save_receivers(self.save)
+
+        self.save.write('\nEXP TWO \n')
+        self.save.write('REC_MAN\n')
+        exp2.save_receivers(self.save)
+        self.save.write('SUI_WOMAN\n')
+        exp2.save_suitors(self.save)
+
+        print 'SUI_MAN:'
+        exp1.print_suitors()
+        print 'REC_WOM:'
+        exp1.print_receivers()
+        print '               '
+        print 'REC_MAN:'
+        exp2.print_receivers()
+        print 'SUI_WOM:'
+        exp2.print_suitors()
+
+if __name__ == '__main__':
+    exp = experiment(20)
+    exp.write_init_condition()
+    exp.unidirectional_match_both_sides()
+    exp.end()

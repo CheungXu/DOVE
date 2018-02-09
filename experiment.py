4899a8105a09eb456419d9d0196fbdf339fb4e4e
CheungXu <cheungxu_0218@foxmail.com>
2018-02-09 20:33:18 +0800

    ZX-2018.2.9
    （1）增加了节点随机属性及评价权重，添加了随机属性/权重生成器。节点根据属性和权重生成匹配列表。并更新了相应匹配算法。
    （2）增加了log日志记录。

--- experiment.py
+++ experiment.py
@@ -3,77 +3,67 @@ import dove
 import copy
 
 class Experiments(object):
-    def __init__(self,person_num = 20):
+    def __init__(self,person_num = 20,feature_num = 3):
         self.person_num = person_num
+        self.feature_num = feature_num
         self.save_path = '.\\data\\test.txt'
         self.save = open(self.save_path,'w')
-        random = dove.List_randomer(0,self.person_num)
-        self.mans_list = []
-        self.womans_list = []
-        for i in range(self.person_num):
-            self.mans_list.append(copy.copy(random.create_list()))
-            self.womans_list.append(copy.copy(random.create_list()))
+        self.man = dove.create_Person(person_num,feature_num,person_num)
+        self.woman = dove.create_Person(person_num,feature_num,person_num)
 
     def __del__(self):
         self.person_num  = 0
         self.save_path = ''
         self.save.close()
-        self.mans_list = []
-        self.womans_list = []
         
-    def write_init_condition(self):
-        self.save.write('MAN_LIST\n')
-        for i in range(len(self.mans_list)):
-            for j in range(len(self.mans_list[i])):
-                self.save.write(str(self.mans_list[i][j]))
-                self.save.write(' ')
-            self.save.write('\n')
-        self.save.write('\nWOMAN_LIST\n')
-        for i in range(len(self.womans_list)):
-            for j in range(len(self.womans_list[i])):
-                self.save.write(str(self.womans_list[i][j]))
-                self.save.write(' ')
-            self.save.write('\n')
+    def write_init_condition(self, matching):
+        matching.save_init_information(self.save)
 
-    def __unidirectional_match__(self,suitor,receiver):
-        suitor_list = copy.deepcopy(suitor)
-        receiver_list = copy.deepcopy(receiver)
-        suitor_result = dove.create_Suitors(suitor_list,5)
-        receiver_result = dove.create_Receivers(receiver_list,5)
-        experiment = dove.Experiment(suitor_result,receiver_result)
-        experiment.start()
-        return suitor_result,receiver_result,experiment
-
-    def unidirectional_match_both_sides(self):
-        suitor_man,receiver_woman,exp1 = self.__unidirectional_match__(self.mans_list,self.womans_list)
-        suitor_woman,receiver_man,exp2 = self.__unidirectional_match__(self.womans_list,self.mans_list)
-
+    def unidirectional_match(self):
+        matching = dove.Matching(self.man,self.woman)
+        self.write_init_condition(matching)
+        matching.start()
         self.save.write('\nEXP ONE \n')
         self.save.write('SUI_MAN\n')
-        exp1.save_suitors(self.save)
+        matching.save_suitors(self.save)
         self.save.write('REC_WOMAN\n')
-        exp1.save_receivers(self.save)
+        matching.save_receivers(self.save)
+        
+        return matching
+
+    def unidirectional_match_both_sides(self):
+        matching_forward = dove.Matching(self.man,self.woman)
+        matching_backward = dove.Matching(self.woman,self.man)
+        self.write_init_condition(matching_forward)
+        self.write_init_condition(matching_backward)
+        matching_forward.start()
+        matching_backward.start()
+        self.save.write('\nEXP ONE \n')
+        self.save.write('SUI_MAN\n')
+        matching_forward.save_suitors(self.save)
+        self.save.write('REC_WOMAN\n')
+        matching_forward.save_receivers(self.save)
 
         self.save.write('\nEXP TWO \n')
         self.save.write('REC_MAN\n')
-        exp2.save_receivers(self.save)
+        matching_backward.save_receivers(self.save)
         self.save.write('SUI_WOMAN\n')
-        exp2.save_suitors(self.save)
+        matching_backward.save_suitors(self.save)
 
         print 'SUI_MAN:'
-        exp1.print_suitors()
+        matching_forward.print_suitors()
         print 'REC_WOM:'
-        exp1.print_receivers()
+        matching_forward.print_receivers()
         print '               '
         print 'REC_MAN:'
-        exp2.print_receivers()
+        matching_backward.print_receivers()
         print 'SUI_WOM:'
-        exp2.print_suitors()
-            
+        matching_backward.print_suitors()
+
+        return matching_forward, matching_backward
 
             
 if __name__ == '__main__':
-    exp = Experiments(20)
-    exp.write_init_condition()
-    exp.unidirectional_match_both_sides()
+    exp = Experiments(20,3)
+    exp.unidirectional_match()
     del exp
